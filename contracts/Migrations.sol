ae39e9025d154608fa88dbe674ca730c7459c80c
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-28 15:55:33 +0100

    Refactor Migrations contract and update truffle-config.js

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,7 +1,24 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
 contract Migrations{
     address public owner;
     uint public last_completed_migration;
+
+    constructor() {
+        owner = msg.sender;
+    }
+modifier restricted() {
+        if (msg.sender == owner) _; // the _; is replaced by the function code 
+} 
+
+function setCompleted(uint completed) public restricted{
+last_completed_migration = completed;
+
+}
+
+function upgrade(address newAddress) public restricted{
+ Migrations upgraded = Migrations(newAddress); // create a new contract instance of the Migrations contract
+ upgraded.setCompleted(last_completed_migration); // call the setCompleted function of the new contract instance
+}
 }
