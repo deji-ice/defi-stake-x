23c6f72efec98b5b8ade1239cbff0d90814c0c79
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-08 03:31:12 +0100

    Add DecentralBank contract: implement basic structure with Tether and RewardToken integration; update Migrations.json for correct referencedDeclaration values

---
+++ contracts/DecentralBank.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "./Tether.sol";
+import "./RewardToken.sol";
+
+contract DecentralBank {
+    address public owner;
+    string public name = "Decentral Bank";
+
+    Tether public tether;
+    RewardToken public rewardToken;
+
+    constructor(RewardToken _rwd, Tether _tether ) public {
+        owner = msg.sender;
+        rwd = _rwd; // Reward Token
+        tether = _tether; // Tether Token 
+    }
+}
