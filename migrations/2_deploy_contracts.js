644ba572b1ec72ae602a075e720bba9dcae4660c
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-09 03:31:22 +0100

    Deploy Tether, RewardToken, and DecentralBank contracts; transfer initial tokens to accounts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,25 @@
 const Tether = artifacts.require("Tether");
+const RewardToken = artifacts.require("RewardToken");
+const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function (deployer) {
+// Deploy the Tether, RewardToken, and DecentralBank contracts to the network specified in truffle-config.js (development by default)
+module.exports = async function (deployer, network, accounts) {
   // Deploy the Tether contract
-  await deployer.deploy(Tether); 
-}
+  await deployer.deploy(Tether);
+  // Get the deployed Tether contract
+  const tether = await Tether.deployed();
+  // Deploy the RewardToken contract
+  await deployer.deploy(RewardToken);
+  // Get the deployed RewardToken contract
+  const rwd = await RewardToken.deployed();
+  // Deploy the Decentral Bank contract
+  await deployer.deploy(DecentralBank);
+  // Get the deployed Decentral Bank contract
+  const dbank = await DecentralBank.deployed();
+  // transfer all the RewardToken tokens to the Decentral Bank contract
+  await rwd.transfer(dbank.address, "1000000000000000000000000");
+
+  // transfer all the Tether tokens to the first account
+  await tether.transfer(accounts[1], "1000000000000000000000000");
+
+};
