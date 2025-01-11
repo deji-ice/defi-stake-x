7d008a3214295a824376a2a020b263da254bc9ba
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-01-11 11:26:13 +0100

    Enhance DecentralBank tests: add RewardToken deployment and assertions for token names

--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -1,19 +1,32 @@
-
-
 const Tether = artifacts.require("Tether");
-// const RewardToken = artifacts.require("RewardToken");
-// const DecentralBank = artifacts.require("DecentralBank");
+const RewardToken = artifacts.require("RewardToken");
+const DecentralBank = artifacts.require("DecentralBank");
 
-require("chai")
-    .use(require("chai-as-promised"))
-    .should();
+require("chai").use(require("chai-as-promised")).should();
 
 contract("DecentralBank", (accounts) => {
-    describe("Mock Tether Deployment", async () => {
-        it("matches the name succefully", async ()=>{
-            let tether = await Tether.new();
-            const name = await tether.name();
-            assert.equal(name, "Test Tether Token");
-        })
-    })
+  let tether, rwd, dbank;
+  before(async () => {
+    tether = await Tether.new();
+    rwd = await RewardToken.new();
+    dbank = await DecentralBank.new(rwd.address, tether.address);
+  });
+  describe("Mock Tether Deployment", async () => {
+    it("matches the name succefully", async () => {
+      const name = await tether.name();
+      assert.equal(name, "Test Tether Token");
+    });
+  });
+  describe("Reward Token Deployment", async () => {
+    it("matches the Reward Token name succefully", async () => {
+      const name = await rwd.name();
+      assert.equal(name, "Icey Token");
+    });
+  });
+  describe("Decentral Bank Deployment", async () => {
+    it("matches the Decentral Bank name succefully", async () => {
+      const name = await dbank.name();
+      assert.equal(name, "Decentral Bank");
+    });
+  });
 });
