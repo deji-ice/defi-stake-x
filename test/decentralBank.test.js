74b491aab7f19b79240697ed43cbc6cb2c945391
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-02-05 12:39:35 +0100

    Enhance DecentralBank tests: add comments for clarity and transfer 100 mock Tether tokens to customer

--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -4,10 +4,16 @@ const DecentralBank = artifacts.require("DecentralBank");
 
 require("chai").use(require("chai-as-promised")).should();
 
-contract("DecentralBank", (accounts) => {
+// Test the DecentralBank contract
+// The contract is deployed with the following parameters: RewardToken address and Tether address
+// The contract is deployed by the owner
+// The owner is the first account in the accounts array
+// The customer is the second account in the accounts array
+contract("DecentralBank", ([owner, customer]) => {
   let tether, rwd, dbank;
+  // Helper function to convert tokens to wei (1 token = 10^18 wei)
   const tokens = (number) => web3.utils.toWei(number, "ether");
-  
+
   // Before each test, deploy the contract with the following parameters and assign the contract to the variables
   before(async () => {
     tether = await Tether.new();
@@ -16,6 +22,10 @@ contract("DecentralBank", (accounts) => {
 
     // Transfer all tokens to Decentral Bank (1 million)
     await rwd.transfer(dbank.address, tokens("1000000"));
+
+    //transfer 100 mock Tether tokens to customer
+
+    await tether.transfer(customer, tokens("100"), { from: owner });
   });
 
   describe("Mock Tether Deployment", async () => {
