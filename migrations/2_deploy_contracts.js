dd1f56824b98bde8d228c7c77218f59dd54a63b5
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-02-05 12:09:08 +0100

    Refactor contract deployment script: switch to ES6 imports, adjust Tether deployment with initial supply, and transfer total supply of RewardToken to Decentral Bank

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
-const Tether = artifacts.require("Tether");
-const RewardToken = artifacts.require("RewardToken");
-const DecentralBank = artifacts.require("DecentralBank");
+import Tether from "Tether";
+import RewardToken from "RewardToken";
+import DecentralBank from "DecentralBank";
 
 // Deploy the Tether, RewardToken, and DecentralBank contracts to the network specified in truffle-config.js (development by default)
-module.exports = async function (deployer, network, accounts) {
-  // Deploy the Tether contract
-  await deployer.deploy(Tether);
+// Note: The deployment order is important due to dependencies between contracts
+export default async function (deployer, accounts) {
+  // accounts: Array of addresses provided by the Ethereum client
+  // Deploy the Tether contract with an initial supply of 1,000,000 Tether tokens
+  await deployer.deploy(Tether, "1000000000000000000000000");
   // Get the deployed Tether contract
   const tether = await Tether.deployed();
   // Deploy the RewardToken contract
@@ -16,8 +18,12 @@ module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(DecentralBank, rwd.address, tether.address);
   // Get the deployed Decentral Bank contract
   const dbank = await DecentralBank.deployed();
-  // transfer all the RewardToken tokens to the Decentral Bank contract
-  await rwd.transfer(dbank.address, "1000000000000000000000000");
+
+
+  // Check the total supply of RewardToken
+  const totalSupply = await rwd.totalSupply();
+  // Transfer the total supply to Decentral Bank
+  await rwd.transfer(dbank.address, totalSupply.toString());
 
   // transfer 100 Tether tokens to accounts[1]
   await tether.transfer(accounts[1], "100000000000000000000");
